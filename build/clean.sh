1a831e2d8bbdb76a60ad35218e78ca27383bda68
Jason Reicheneker <jreicheneker@users.noreply.github.com>
2015-08-16 10:25:37 -0600

    Improve clean up in clean.sh and makefiles.

--- build/clean.sh
+++ build/clean.sh
@@ -33,6 +33,9 @@ make -C ../native/so/DVBCapture2.0 clean
 make -C ../native/so/FirewireCapture clean
 make -C ../native/so/MPEGParser2.0 clean
 make -C ../native/so/HDHomeRun2.0 clean
+make -C ../native/ax/Native2.0/NativeCore clean
+make -C ../native/ax/Channel-2 clean
+make -C ../native/ax/TSnative clean
 
 make -C ../native/so/PVR150Tuning clean
 make -C ../native/so/DirecTVSerialControl clean
@@ -40,20 +43,25 @@ make -C ../native/so/FirewireTuning clean
 
 make -C ../third_party/swscale clean
 
-make -C ../third_party/codecs/giflib clean
-make -C ../third_party/codecs/jpeg-6b clean
-make -C ../third_party/codecs/libpng clean
-make -C ../third_party/codecs/tiff clean
+make -C ../third_party/codecs/giflib distclean
+make -C ../third_party/codecs/jpeg-6b distclean
+make -C ../third_party/codecs/libpng distclean
+make -C ../third_party/codecs/tiff distclean
 
 make -C ../third_party/SageTV-LGPL/imageload clean
 make -C ../native/crosslibs/Freetype clean
 
-make -C ../third_party/codecs/faac clean
-make -C ../third_party/codecs/ffad2 clean
-make -C ../third_party/codecs/xvidcore/build/general clean
-make -C ../third_party/codecs/x264 clean
+# faac and faad2 don't clean up after themselves very well
+make -C ../third_party/codecs/faac maintainer-clean
+  find ../third_party/codecs/faac -name "Makefile.in"  -exec rm {} \;
+  for i in aclocal.m4 compile config.guess config.h.in config.sub configure depcomp install-sh ltmain.sh missing; do rm ../third_party/codecs/faac/$i; done
+make -C ../third_party/codecs/faad2 maintainer-clean
+  find ../third_party/codecs/faad2 -name "Makefile.in"  -exec rm {} \;
+  for i in aclocal.m4 compile config.guess config.h.in config.sub configure depcomp INSTALL install-sh ltmain.sh missing; do rm ../third_party/codecs/faad2/$i; done
+make -C ../third_party/codecs/xvidcore/build/generic mrproper
+make -C ../third_party/codecs/x264 distclean
 
-make -C ../third_party/ffmpeg clean
-make -C ../third_party/mplayer clean
+make -C ../third_party/ffmpeg distclean
+make -C ../third_party/mplayer distclean
 
 make -C ../native/so/Mpeg2Transcoder clean
