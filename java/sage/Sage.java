973a47263cc40cfb39a41dcf07889e86a2061f5f
enternoescape <dcagent17@yahoo.com>
2016-07-20 06:37:18 -0400

    Incremented version.

--- java/sage/Sage.java
+++ java/sage/Sage.java
@@ -129,7 +129,7 @@ public final class Sage
 
   public static final byte CLIENT_COMPATIBLE_MAJOR_VERSION = 9;
   public static final byte CLIENT_COMPATIBLE_MINOR_VERSION = 0;
-  public static final byte CLIENT_COMPATIBLE_MICRO_VERSION = 1;
+  public static final byte CLIENT_COMPATIBLE_MICRO_VERSION = 0;
 
   public static final byte ENCODER_COMPATIBLE_MAJOR_VERSION = 4;
   public static final byte ENCODER_COMPATIBLE_MINOR_VERSION = 1;
