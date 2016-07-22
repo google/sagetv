c42b8758e19afca433e7578f47f9ec8b73697b32
enternoescape <dcagent17@yahoo.com>
2016-07-22 07:09:06 -0400

    Refactored Gson 2.7 release to sage.epg.sd.gson.

---
+++ java/sage/epg/sd/gson/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides annotations that can be used with {@link sage.epg.sd.gson.Gson}.
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package sage.epg.sd.gson.annotations;
---
+++ java/sage/epg/sd/gson/internal/bind/SqlDateTypeAdapter.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sage.epg.sd.gson.internal.bind;
+
+import sage.epg.sd.gson.Gson;
+import sage.epg.sd.gson.JsonSyntaxException;
+import sage.epg.sd.gson.TypeAdapter;
+import sage.epg.sd.gson.TypeAdapterFactory;
+import sage.epg.sd.gson.reflect.TypeToken;
+import sage.epg.sd.gson.stream.JsonReader;
+import sage.epg.sd.gson.stream.JsonToken;
+import sage.epg.sd.gson.stream.JsonWriter;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/**
+ * Adapter for java.sql.Date. Although this class appears stateless, it is not.
+ * DateFormat captures its time zone and locale when it is created, which gives
+ * this class state. DateFormat isn't thread safe either, so this class has
+ * to synchronize its read and write methods.
+ */
+public final class SqlDateTypeAdapter extends TypeAdapter<java.sql.Date> {
+  public static final TypeAdapterFactory FACTORY = new TypeAdapterFactory() {
+    @SuppressWarnings("unchecked") // we use a runtime check to make sure the 'T's equal
+    @Override public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> typeToken) {
+      return typeToken.getRawType() == java.sql.Date.class
+          ? (TypeAdapter<T>) new SqlDateTypeAdapter() : null;
+    }
+  };
+
+  private final DateFormat format = new SimpleDateFormat("MMM d, yyyy");
+
+  @Override
+  public synchronized java.sql.Date read(JsonReader in) throws IOException {
+    if (in.peek() == JsonToken.NULL) {
+      in.nextNull();
+      return null;
+    }
+    try {
+      final long utilDate = format.parse(in.nextString()).getTime();
+      return new java.sql.Date(utilDate);
+    } catch (ParseException e) {
+      throw new JsonSyntaxException(e);
+    }
+  }
+
+  @Override
+  public synchronized void write(JsonWriter out, java.sql.Date value) throws IOException {
+    out.value(value == null ? null : format.format(value));
+  }
+}
