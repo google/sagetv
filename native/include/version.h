7912487d42f7db28abe1a2bbad409db87338cc15
jusjoken <usjusjoken@gmail.com>
2018-10-15 11:48:50 -0500

    Release for version 9.1.10 [ci release]
    Added missing version files and small change in installer script

--- native/include/version.h
+++ native/include/version.h
@@ -1,10 +1,10 @@
-/* Created by InstallerBuild.ps1 script 18-May-2018 19:11 */
+/* Created by InstallerBuild.ps1 script 13-Oct-2018 15:38 */
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
 
 #define VERSION_MAJOR               9
 #define VERSION_MINOR               1
-#define VERSION_REVISION            9
+#define VERSION_REVISION            10
 
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION
 #define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
