a3f8ef929d46f46aec863f8d88150bf5a6afe31c
Warren Juenemann <warren.juenemann@gmail.com>
2020-05-15 17:18:31 -0700

    Release for version 9.2.2 [ci release]

--- native/include/version.h
+++ native/include/version.h
@@ -1,10 +1,10 @@
-/* Created by InstallerBuild.ps1 script 24-Mar-2019 13:55 */
+/* Created by InstallerBuild.ps1 script 16-May-2020 18:30 */
 #define STRINGIZE2(s) #s
 #define STRINGIZE(s) STRINGIZE2(s)
 
 #define VERSION_MAJOR               9
 #define VERSION_MINOR               2
-#define VERSION_REVISION            1
+#define VERSION_REVISION            2
 
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION
 #define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
